797a9d8643d648c5317c719b77d4d44df22f875a
Chen Xinyang <chenxy1999728@126.com>
2021-05-31 08:34:09 +0800

    Env updates

--- src/main.js
+++ src/main.js
@@ -3,7 +3,22 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import store from './store'
 
+import '@mdi/font/css/materialdesignicons.css'
+
 Vue.config.productionTip = false
+
+import VuetifyDialog from 'vuetify-dialog'
+import 'vuetify-dialog/dist/vuetify-dialog.css'
+Vue.use(VuetifyDialog, {
+  context: {
+    vuetify
+  }
+})
+
+import BaiduMap from 'vue-baidu-map'
+Vue.use(BaiduMap, {
+  ak: process.env.VUE_APP_BAIDU_MAP_AK
+})
 
 new Vue({
   vuetify,
